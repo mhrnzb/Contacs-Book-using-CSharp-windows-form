d27a112993e4f16589092a089f87559ecd09060c
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-17 15:47:33 +0330

    make a class to implement interface

---
+++ ContactsBook/ContactsBook/Services/ContactsRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ContactsBook
+{
+    class ContactsRepository : IContactsRepository
+    {
+        public bool Delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int contactId, string name, string family, string mobile, string email, int age, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
