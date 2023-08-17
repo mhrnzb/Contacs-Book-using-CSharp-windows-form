2ff5faf1bb9050198f3f4e8d70e9dcad64faab67
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-17 15:42:44 +0330

    make interface to set data base rules

---
+++ ContactsBook/ContactsBook/Repository/IContactsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ContactsBook.Repository
+{
+    internal interface IContactsRepository
+    {
+        DataTable SelectAll();
+        DataTable SelectRow(int contactId);
+        bool Insert(string name, string family, string mobile, string email, int age, string address);
+        bool Update (int contactId, string name, string family, string mobile, string email, int age, string address);
+
+        bool Delete (int contactId);
+
+    }
+}
